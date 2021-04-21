95bdf8bd11b623426c4c47244f79b2396ba05498
TechnoBoy <Dloadjunior@gmail.com>
2021-04-21 21:36:57 +0530

    OpenCart Installation

---
+++ OpenCart/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/auto/public_html/OpenCart/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/auto/public_html/OpenCart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/auto/public_html/OpenCart/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1/auto/public_html/OpenCart/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/Program Files/EasyPHP 3.0/www/auto/public_html/OpenCart/admin/');
+define('DIR_SYSTEM', 'E:/Program Files/EasyPHP 3.0/www/auto/public_html/OpenCart/system/');
+define('DIR_IMAGE', 'E:/Program Files/EasyPHP 3.0/www/auto/public_html/OpenCart/image/');
+define('DIR_STORAGE', 'E:/Program Files/EasyPHP 3.0/www/auto/ocartdata/storage/');
+define('DIR_CATALOG', 'E:/Program Files/EasyPHP 3.0/www/auto/public_html/OpenCart/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'pulkit_ocar872');
+define('DB_PASSWORD', '(5SS05]2Vp');
+define('DB_DATABASE', 'pulkit_ocar872');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ocio_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ OpenCart/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/auto/public_html/OpenCart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/auto/public_html/OpenCart/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/Program Files/EasyPHP 3.0/www/auto/public_html/OpenCart/catalog/');
+define('DIR_SYSTEM', 'E:/Program Files/EasyPHP 3.0/www/auto/public_html/OpenCart/system/');
+define('DIR_IMAGE', 'E:/Program Files/EasyPHP 3.0/www/auto/public_html/OpenCart/image/');
+define('DIR_STORAGE', 'E:/Program Files/EasyPHP 3.0/www/auto/ocartdata/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'pulkit_ocar872');
+define('DB_PASSWORD', '(5SS05]2Vp');
+define('DB_DATABASE', 'pulkit_ocar872');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ocio_');
