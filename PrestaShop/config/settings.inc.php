e80c3801e50a98161682178081555b38998fc35b
TechnoBoy <Dloadjunior@gmail.com>
2021-04-21 21:16:02 +0530

    PrestaShop Installation

---
+++ PrestaShop/config/settings.inc.php
@@ -0,0 +1,14 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'pulkit_pres235');
+define('_DB_USER_', 'pulkit_pres235');
+define('_DB_PASSWD_', 'S3Sp!1.03D');
+define('_DB_PREFIX_', 'psm1_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'iegzwnfl1pmlujdmod3ulesn2vihct33cjwpykhifavlgnkcvfp6yaf4');
+define('_COOKIE_IV_', 'kta9ctd9');
+define('_PS_CREATION_DATE_', '2021-04-21');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.24');
